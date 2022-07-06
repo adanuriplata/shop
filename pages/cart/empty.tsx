a78a5194fee8fbc2c60ef634448a4ea8d795995d
distordev <adanuriplata@gmail.com>
2022-07-06 18:13:52 -0500

    Feat: Added checkout and login page ui

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,26 @@
+import {ShopLayout} from "../../components/layouts";
+import {Box, Link, Typography} from "@mui/material";
+import {RemoveShoppingCartOutlined} from "@mui/icons-material"
+import NextLink from "next/link";
+
+
+const EmptyPage = () => {
+  return (
+    <ShopLayout title='Carrito vacio' pageDescription='No hay articulos en el carrito'>
+      <Box display='flex' justifyContent='center' alignItems='center' height='calc(100vh - 200px)'>
+
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+        <Box display='flex' flexDirection='column' alignItems='center'>
+          <Typography>Su carrito esta vacio</Typography>
+          <NextLink href='/' passHref>
+            <Link typography='h4' color='secondary'>
+              Regresar
+            </Link>
+          </NextLink>
+        </Box>
+      </Box>
+    </ShopLayout>
+  )
+}
+
+export default EmptyPage
---
+++ pages/orders/history.tsx
@@ -0,0 +1,68 @@
+import {ShopLayout} from "../../components/layouts";
+import {Chip, Link, Grid, Typography} from "@mui/material";
+import {DataGrid, GridColDef, GridValueGetterParams} from "@mui/x-data-grid";
+import NextLink from "next/link";
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 100},
+  { field: 'fullname', headerName: 'Nombre Completo', width: 300},
+  { field: 'paid',
+    headerName: 'Pagada',
+    description: 'Muestra información si está pagada la orden',
+    width: 200,
+    renderCell: (params: GridValueGetterParams) => {
+      return (
+        params.row.paid
+          ? <Chip color='success' label='Pagada' variant='outlined' />
+          : <Chip color='error' label='No Pagada' variant='outlined' />
+      )
+    }
+  },
+  { field: 'Orden',
+    headerName: 'Ver Orden',
+    sortable: false,
+    width: 200,
+    renderCell: (params: GridValueGetterParams) => {
+      return (
+        <NextLink href={`/orders/${ params.row.id }`}>
+          <Link>
+            Ver Orden
+          </Link>
+        </NextLink>
+      )
+    }
+  },
+
+]
+
+const rows = [
+  { id: 1, paid: true, fullname: 'Fernando H'},
+  { id: 2, paid: true, fullname: 'Julian Casablancas'},
+  { id: 3, paid: false, fullname: 'Elias Mauri'},
+  { id: 4, paid: true, fullname: 'Sara Morales'},
+  { id: 5, paid: false, fullname: 'Salvador Reyes'},
+  { id: 6, paid: true, fullname: 'Alberto Garcia'},
+]
+
+const HistoryPage = () => {
+  return (
+    <ShopLayout title='Historial de ordenes' pageDescription='Historial de ordenes de cliente'>
+      <Typography variant='h1' component='h1'>Historial deordenes</Typography>
+
+      <Grid container>
+        <Grid item xs={ 12 } sx={{ height:650, width: '100%'}}>
+          <DataGrid
+            rows={ rows }
+            columns={ columns }
+            pageSize={ 10 }
+            rowsPerPageOptions={ [10] }
+          >
+
+          </DataGrid>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  )
+}
+
+export default HistoryPage
